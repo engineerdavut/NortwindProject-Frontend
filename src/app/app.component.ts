d4114790c7e3c879da36d9178a8777953be4f405
Davut Aktaş <engineerdavut@gmail.com>
2021-04-05 20:55:32 +0300

    17.ders

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-
+//( obje demek) component annotacion typscript () sonra javascript e dönüştürüyor.
+// [] array demek birden fazla css  kullanabilirsin demek.
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'northwind';
+  // typescript tip güvenli olduğu için string parametresini destekliyor. tek tırnak cift tınak farketmez.
+  //title:string="northeind"; sonuna noktalı virgül koymayabilirsin.
+  title: string = 'northwind';
+  user: string = 'davut';
+  //any hersey olabilir {} object demek.
+
 }
